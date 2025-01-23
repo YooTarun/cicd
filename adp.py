1300dae9d3cc12b36575aa52be13a433ca56553d
YooTarun <gangadhartarun21@gmail.com>
2025-01-23 21:07:25 +0530

    added new endpoint

--- adp.py
+++ adp.py
@@ -10,3 +10,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/greet/{name}")
+def greet_user(name: str):
+    return {"greeting": f"Hello, {name}!"}
